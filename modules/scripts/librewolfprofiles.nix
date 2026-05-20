7d8aa54bbbc68f8b9d96447b1d6df8d848457141
Tomas Rivera <tomas.riveral@icloud.com>
2026-05-20 17:07:44 +0200

    Librewolf: add private window in profile selector

--- modules/scripts/librewolfprofiles.nix
+++ modules/scripts/librewolfprofiles.nix
@@ -12,7 +12,7 @@ writeShellApplication {
   ];
   text = ''
     # List of profiles
-    profiles=("work" "other")
+    profiles=("work" "other" "private window")
 
     # Use fzf to select
     selected=$(printf "%s\n" "''${profiles[@]}" | fzf --height 6 --reverse --prompt="Select LibreWolf profile:")
@@ -21,9 +21,13 @@ writeShellApplication {
     [[ -z "$selected" ]] && exit 0
 
     # Launch LibreWolf detached, keeping environment
-    setsid  librewolf -P "$selected" --no-remote >/dev/null 2>&1 &
-        pkill -f "kitty.*Select LibreWolf profile"
-
+    if [[ "$selected" == "private window" ]]; then
+      setsid  librewolf --private-window --no-remote >/dev/null 2>&1 &
+          pkill -f "kitty.*Select LibreWolf profile"
+    else
+      setsid  librewolf -P "$selected" --no-remote >/dev/null 2>&1 &
+          pkill -f "kitty.*Select LibreWolf profile"
+    fi
 
     # Exit the kitty terminal after selection
     exit
